aa6189e9b237a9519a506109be18e4dc9a667251
taswyn <taswyn@gmail.com>
2020-03-26 04:20:39 +0000

    added "explosion" function

--- predator-numbers-disp.py
+++ predator-numbers-disp.py
@@ -4,6 +4,8 @@ Requires Pillow (Python Imaging Library fork)
 """
 
 import time
+import random
+import math
 
 import board
 import digitalio
@@ -22,12 +24,14 @@ adafruit_blinka.microcontroller.bcm283x.pin.i2cPorts = (
     (5, pins.D22, pins.D18), (4, pins.D24, pins.D23), (3, pins.D27, pins.D17), (1, pins.SCL, pins.SDA), (0, pins.D1, pins.D0),   
 )
 
+
+
 def displayInit(oledDisplay):
     oledDisplay.fill(0)
     oledDisplay.show()
     return
 
-def runCountdown(oledDisplay, displayNumber, sequenceStep):
+def runCountdown(oledDisplay, displayNumber, sequenceStep) :
 
     if sequenceStep < sequenceLast[displayNumber] :
 
@@ -45,6 +49,27 @@ def runCountdown(oledDisplay, displayNumber, sequenceStep):
 
     oledDisplay.show()
     return
+
+def runExplosion(oledDisplays) :
+    center = [63,31]
+
+    oledImages = [Image.new('1', (oledDisplay.width, oledDisplay.height)) for oledDisplay in oledDisplays]
+
+    drawObjects = [ImageDraw.Draw(oledImage) for oledImage in oledImages]
+
+    for i in range(1, 15, 2) :
+        for displayIndex, oledDisplay in enumerate(oledDisplays) :
+            plotPoints = []
+            for randomi in range (0, 30 * i * math.floor(i/2)) :
+                x = random.randrange(-6*i, 6*i) + center[0]
+                y = random.randrange(math.floor(-5*i), math.floor(5*i)) + center[1]
+                plotPoints.append((x,y)) 
+            
+            drawObjects[displayIndex].point(plotPoints, fill=255)
+
+            oledDisplay.image(oledImages[displayIndex])
+            oledDisplay.show()
+
 
 busDictionary = { 
 1 : { 'SDA': pins.SDA, 'SCL': pins.SCL }, 
@@ -72,4 +97,6 @@ while totalAdvances < 10 :
     # note that for running indefinitely, we'll need a way to zero totalAdvances to prevent overflow (hence not a for loop)
     time.sleep(delayInterval)
     pass
+
+runExplosion(displayList)
 
